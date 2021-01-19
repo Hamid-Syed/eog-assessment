87674cac54b28cc54d372afc9c6a6c443062cc07
Hamid <hamidsyedd@gmail.com>
2021-01-19 17:52:40 -0500

    Added graph ui with sample data

--- src/Features/Charts/Charts.tsx
+++ src/Features/Charts/Charts.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
 import {Grid, Card} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 import {ISelectedMetrics, IDropdown} from './../utils/types';
-
+import {ResponsiveContainer,LineChart,XAxis,YAxis,Tooltip,Line,CartesianGrid} from 'recharts';
+const useStyles = makeStyles({
+    chartContainer:{
+        padding:20
+    }
+});
 const Charts = ({selectedMetrics}:ISelectedMetrics) => {
-    console.log(selectedMetrics);
+    const classes = useStyles();
+    const data = [{name:'A',metric:10},{name:'B',metric:5},{name:'C',metric:30},{name:'D',metric:20}];
+
     return (
-        <Grid container>
-            <Grid item xs={12}>
+        <Grid container className={classes.chartContainer}>
+            <Grid item xs={12} >
            {
                selectedMetrics && selectedMetrics.map((metric:IDropdown) => (<Card>{metric.label}</Card>))
            }            
             </Grid>
-            <Grid item xs={12}>
-                Charts content
+            <Grid item xs={12} >
+                <ResponsiveContainer width="100%" height={400}>
+                    <LineChart data={data}>
+                        <XAxis dataKey="name"/>
+                        <YAxis/>
+                        <Tooltip/>
+                        <CartesianGrid strokeDasharray="3 3"/>
+                        <Line dataKey="metric" type="monotone" stroke="#053c6d"/>
+                    </LineChart>
+                </ResponsiveContainer>
             </Grid>
         </Grid>
     )
--- src/Features/Charts/ChartsContainer.tsx
+++ src/Features/Charts/ChartsContainer.tsx
@@ -1,16 +1,23 @@
 import React,{useState} from 'react';
 import {Grid} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 import Select from 'react-select';
 import Charts from './Charts';
 const options = [{label:'One',value:1},{label:'Two',value:2}];
-const ChartsContainer:React.FC<{}> = ({})=>{
+const useStyles = makeStyles({
+    container:{
+        padding:20
+    }
+});
+const ChartsContainer = () => {
+    const classes = useStyles();
     const [selectedOption,setSelectedOption] = useState(null);
     const handleDropdownChange = (option:any)=>{
         setSelectedOption(option);
     }
     return (
     
-    <Grid  container className="charts-container">
+    <Grid  container className={classes.container}>
         <Grid item xs={10}>
             Live Charts
         </Grid>
